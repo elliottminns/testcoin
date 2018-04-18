0a963887334bc6e046278fec8c329bff22605f0c
Elliott Minns <elliott.minns@me.com>
2018-04-18 13:27:10 -0500

    Removed Checkpoints and changed chain params

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -96,11 +96,6 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        vSeeds.push_back(CDNSSeedData("0","54.172.62.103"));
-        vSeeds.push_back(CDNSSeedData("1","node.friendshipcoin.com"));
-
-        convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
-
         nPoolMaxTransactions = 3;
         strDarksendPoolDummyAddress = "f25gtaAQYBUNjYrxzZqNdyADSmwPPpWoLC";
         nLastPOWBlock = 20000;
